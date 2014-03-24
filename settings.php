7938f8362508d6e16279d65357020e95d98bd891
Carl LeBlond <carl@easyochem.com>
2014-03-24 12:10:30 -0400

    Delete settings.php

--- settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die;
-
-if ($ADMIN->fulltree) {
-    $settings->add(new admin_setting_configtext('qtype_easyomech_options', get_string('easyomech_options', 'qtype_easyomech'),
-                   get_string('configeasyomechoptions', 'qtype_easyomech'), '', PARAM_TEXT));
-}
-
