550ae810221e3588e8f09ef0efed79716f4037e2
Carl LeBlond <carl@easyochem.com>
2014-03-24 12:22:30 -0400

    Update version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2014032400;
-$plugin->requires  = 2011102700;
+$plugin->requires  = 2012062500;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_easyomech';
 $plugin->maturity  = MATURITY_STABLE;
