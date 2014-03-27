b9e7d897d6cf5771ee2d52053e110201bf26c746
Carl LeBlond <carl.leblond@iup.edu>
2014-03-26 21:09:41 -0400

    cleaning up code

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,4 +1,18 @@
-<?
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 defined('MOODLE_INTERNAL') || die();
 
 function xmldb_qtype_easyomech_upgrade($oldversion = 0) {
@@ -6,8 +20,6 @@ function xmldb_qtype_easyomech_upgrade($oldversion = 0) {
     global $CFG, $DB;
 
     $dbman = $DB->get_manager();
-
-
 
     if ($oldversion < 2013093000) {
 
@@ -24,14 +36,6 @@ function xmldb_qtype_easyomech_upgrade($oldversion = 0) {
         upgrade_plugin_savepoint(true, 2013093000, 'qtype', 'easyomech');
     }
 
-
-
-
     return true;
-
-
 }
 
-
-
-?>
